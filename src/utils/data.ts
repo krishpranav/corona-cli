d5ac39e85d642ca148fad1e3cf0ce2fddf88ea6c
krishpranav <krisna.pranav@gmail.com>
2021-05-07 16:36:05 +0530

    bug fix

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,18 +3,18 @@ import api from 'novelcovid';
 api.settings({ baseUrl: 'https://disease.sh' });
 
 export default class Data {
-    static async get(country: string): Promise<{data: object}> {
-        let covidData: object = {};
-        if (country == 'all') {
-            covidData = await api.all();
-        } else {
-            covidData = await api.countries({
-                allowNull: false,
-                sort: 'cases',
-                strict: false
-            });
-        }
-
-        return { data: covidData };
+  static async get(country: string): Promise<{data: object}> {
+    let covidData: object = {};
+    if (country === 'all') {
+      covidData = await api.all();
+    } else {
+      covidData = await api.countries({
+        allowNull: false,
+        sort: 'cases',
+        strict: false,
+        country,
+      });
     }
+    return { data: covidData };
+  }
 }
