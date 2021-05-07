08d79a28e35149d23100d4d7ee75c27f3445b2a3
krishpranav <krisna.pranav@gmail.com>
2021-05-07 16:19:39 +0530

    update function for sending api request

---
+++ src/utils/data.ts
@@ -0,0 +1,20 @@
+import api from 'novelcovid';
+
+api.settings({ baseUrl: 'https://disease.sh' });
+
+export default class Data {
+    static async get(country: string): Promise<{data: object}> {
+        let covidData: object = {};
+        if (country == 'all') {
+            covidData = await api.all();
+        } else {
+            covidData = await api.countries({
+                allowNull: false,
+                sort: 'cases',
+                strict: false
+            });
+        }
+
+        return { data: covidData };
+    }
+}
